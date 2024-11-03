67d408999b3aa9f098e77fbe309144b2cb9c9205
Luke Nadur <19835357+intricate@users.noreply.github.com>
2024-11-03 14:16:56 -0500

    Add second golden test for encrypted output

--- test/Test/Crypto/Cipher/Xtea.hs
+++ test/Test/Crypto/Cipher/Xtea.hs
@@ -55,8 +55,12 @@ prop_roundTrip_encryptDecrypt = property $ do
         Right encrypted -> encrypted
 
 -- | Test that 'encrypt' output matches the golden example.
-prop_golden_encrypt :: Property
-prop_golden_encrypt = goldenTestByteString goldenCiphertext "test/golden/xtea/golden.bin"
+prop_golden_example1 :: Property
+prop_golden_example1 = goldenTestByteString goldenExample1 "test/golden/xtea/example1/golden.bin"
+
+-- | Test that 'encrypt' output matches the golden example.
+prop_golden_example2 :: Property
+prop_golden_example2 = goldenTestByteString goldenExample2 "test/golden/xtea/example2/golden.bin"
 
 ------------------------------------------------------------------------------
 -- Generators
@@ -77,13 +81,18 @@ genSymmetricKey =
 goldenSymmetricKey :: Xtea.SymmetricKey
 goldenSymmetricKey = Xtea.SymmetricKey 0x41414141 0x42424242 0x43434343 0x44444444
 
-goldenPlaintext :: ByteString
-goldenPlaintext = "The quick brown fox jumps over the lazy doglolol"
+goldenExample1 :: ByteString
+goldenExample1 =
+  let plaintext = "The quick brown fox jumps over the lazy doglolol"
+  in case Xtea.encrypt goldenSymmetricKey plaintext of
+    Left err -> error $ "impossible: goldenCiphertext1: " <> show err
+    Right x -> x
 
-goldenCiphertext :: ByteString
-goldenCiphertext =
-  case Xtea.encrypt goldenSymmetricKey goldenPlaintext of
-    Left err -> error $ "impossible: goldenEncryptedByteString: " <> show err
+goldenExample2 :: ByteString
+goldenExample2 =
+  let plaintext = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vitae faucibus urna. Etiam lectus tortor, eleifend quis ex in, euismod gravida ante. Quisque non eleifend neque. Ut rhoncus lacinia blandit. Maecenas scelerisque faucibus nibh, eu feugiat lorem. Vestibulum pellentesque porta pretium. Sed suscipit mollis tristique. Nam faucibus tellus lobortis justo ultricies, ac maximus lacus cursus. In pharetra tortor neque, quis maximus ex tempor quis. Nullam imperdiet ipsum sit amet urna malesuada, eu laoreet risus rhoncus. Duis volutpat ultrices libero, ut posuere libero tempor vitae. Nullam placerat lectus ipsum, in dictum justo ultricies eget. Donec ac quam placerat, pharetra erat vitae, eleifend est. Quisque eleifend interdum lacus, id varius eros accumsan at. Duis sodales ligula eget massa mollis, ut consectetur arcu molestie. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Curabitur laoreet faucibus elit id scelerisque. Nam fermentum quis neque at feugiat. Vivamus efficitur pellentesque nisl ut rutrum. Aenean ultrices, ipsum nec auctor bibendum, arcu massa sodales ipsum, quis laoreet dolor ex sit amet nunc. Aliquam eu consequat augue, non gravida lorem. Duis sit amet justo at enim sagittis laoreet vitae in enim. Sed felis sem, venenatis ac elit quis, tincidunt dignissim nunc. Sed sit amet felis vitae tellus dictum tincidunt id ut nisi. Suspendisse id tempor enim. Vivamus egestas lorem nec scelerisque venenatis. Phasellus pretium finibus diam in hendrerit. Donec vitae est nisi. Donec at enim tempor, imperdiet sapien sed, vestibulum ligula. Sed eu tellus quis dui dignissim bibendum. Nullam vulputate tellus ac dui sodales, id accumsan ipsum gravida. Sed eu rutrum justo, eu commodo diam. Suspendisse convallis turpis nec odio convallis dignissim. Nunc viverra leo tellus, vel ullamcorper purus sollicitudin vitae. Suspendisse non neque sit amet arcu bibendum tincidunt. Donec congue ex et vulputate convallis. Pellentesque sodales orci id dui maximus convallis. Duis aliquet, magna sit amet viverra aliquet, massa odio interdum diam, sed hendrerit risus dui eget turpis. Sed ipsum erat, cursus nec tempus in, elementum et mi. Nunc eu viverra turpis, non porttitor est. Nullam bibendum dolor eu justo malesuada, nec gravida ante ultrices. Nam non dolor arcu. Pellentesque ac auctor leo. Sed non massa vel lacus luctus porttitor ac quis velit. Ut nec odio vel est porta malesuada ut et nisl. Proin faucibus gravida diam nec sagittis. Curabitur rhoncus tortor sed consequat porttitor. Sed at convallis nulla. Nulla non massa nec arcu egestas facilisis. Maecenas nisl lorem, consequat et venenatis sit amet, porta non augue. Nullam auctor ac nulla et pulvinar. Duis quis velit nec quam pharetra pellentesque. Duis ultricies ante sit amet magna venenatis, id molestie lacus sagittis. Donec in consequat metus, eu tincidunt arcu. Phasellus blandit, magna sit amet tempor posuere, tortor lorem tempor libero, sed consectetur dui est eu tellus. Sed suscipit nisi ut ante ultricies vestibulum. Proin finibus ante non tempus congue. Nunc elit lacus, ullamcorper eu tempus at, maximus a nibh. Fusce sodales mi eu neque efficitur facilisis vel eget orci. Vivamus nec diam sed massa mollis hendrerit. Ut elementum, est ultricies convallis gravida, tortor nunc efficitur purus, ac consectetur purus orci in elit. Nam at diam eu felis aliquet tincidunt non non sapien. Morbi lacinia ligula massa, quis varius neque rutrum vel. Cras tristique quam ac magna semper, id sollicitudin sem vestibulum. Sed pharetra ante et laoreet ornare. Duis a posuere lectus. Donec scelerisque non ante sodales ullamcorper. In sed euismod purus. Vivamus interdum nulla ac justo dictum sagittis. Proin et dui nec neque pellentesque pretium in eu ex. Aenean nunc nibh, elementum ut tortor ac, fringilla vulputate arcu. Suspendisse rhoncus erat id accumsan pulvinar. Morbi dui quam, interdum vel hendrerit a, imperdiet vel tortor. Integer justo velit, commodo eu ante sed, pulvinar vulputate enim. Nam laoreet aliquet nulla vel feugiat. Donec imperdiet massa eget gra"
+  in case Xtea.encrypt goldenSymmetricKey plaintext of
+    Left err -> error $ "impossible: goldenCiphertext2: " <> show err
     Right x -> x
 
 ------------------------------------------------------------------------------
