42fa2313f98e9555929aa09e328693a646cb8fad
Luke Nadur <19835357+intricate@users.noreply.github.com>
2025-02-03 09:55:33 -0500

    Remove unnecessary indentation in Haddock code blocks

--- src/Crypto/Cipher/Xtea.hs
+++ src/Crypto/Cipher/Xtea.hs
@@ -108,16 +108,16 @@ xteaBlocksToByteString endianness bs = LBS.toStrict (runPut putBlocks)
 -- This function is based on the following C implementation:
 --
 -- @
---   void encipher(unsigned int num_rounds, uint32_t v[2], uint32_t const key[4]) {
---       unsigned int i;
---       uint32_t v0=v[0], v1=v[1], sum=0, delta=0x9E3779B9;
---       for (i=0; i < num_rounds; i++) {
---           v0 += (((v1 \<\< 4) ^ (v1 >> 5)) + v1) ^ (sum + key[sum & 3]);
---           sum += delta;
---           v1 += (((v0 \<\< 4) ^ (v0 >> 5)) + v0) ^ (sum + key[(sum>>11) & 3]);
---       }
---       v[0]=v0; v[1]=v1;
---   }
+-- void encipher(unsigned int num_rounds, uint32_t v[2], uint32_t const key[4]) {
+--     unsigned int i;
+--     uint32_t v0=v[0], v1=v[1], sum=0, delta=0x9E3779B9;
+--     for (i=0; i < num_rounds; i++) {
+--         v0 += (((v1 \<\< 4) ^ (v1 >> 5)) + v1) ^ (sum + key[sum & 3]);
+--         sum += delta;
+--         v1 += (((v0 \<\< 4) ^ (v0 >> 5)) + v0) ^ (sum + key[(sum>>11) & 3]);
+--     }
+--     v[0]=v0; v[1]=v1;
+-- }
 -- @
 encryptBlock :: SymmetricKey -> (Word32, Word32) -> (Word32, Word32)
 encryptBlock k (startingV0, startingV1) = go rounds 0 startingV0 startingV1
@@ -182,16 +182,16 @@ encrypt = encrypt' BigEndian
 -- This function is based on the following C implementation:
 --
 -- @
---   void decipher(unsigned int num_rounds, uint32_t v[2], uint32_t const key[4]) {
---       unsigned int i;
---       uint32_t v0=v[0], v1=v[1], delta=0x9E3779B9, sum=delta*num_rounds;
---       for (i=0; i < num_rounds; i++) {
---           v1 -= (((v0 \<\< 4) ^ (v0 >> 5)) + v0) ^ (sum + key[(sum>>11) & 3]);
---           sum -= delta;
---           v0 -= (((v1 \<\< 4) ^ (v1 >> 5)) + v1) ^ (sum + key[sum & 3]);
---       }
---       v[0]=v0; v[1]=v1;
---   }
+-- void decipher(unsigned int num_rounds, uint32_t v[2], uint32_t const key[4]) {
+--     unsigned int i;
+--     uint32_t v0=v[0], v1=v[1], delta=0x9E3779B9, sum=delta*num_rounds;
+--     for (i=0; i < num_rounds; i++) {
+--         v1 -= (((v0 \<\< 4) ^ (v0 >> 5)) + v0) ^ (sum + key[(sum>>11) & 3]);
+--         sum -= delta;
+--         v0 -= (((v1 \<\< 4) ^ (v1 >> 5)) + v1) ^ (sum + key[sum & 3]);
+--     }
+--     v[0]=v0; v[1]=v1;
+-- }
 -- @
 decryptBlock :: SymmetricKey -> (Word32, Word32) -> (Word32, Word32)
 decryptBlock k (startingV0, startingV1) = go rounds (delta * rounds) startingV0 startingV1
